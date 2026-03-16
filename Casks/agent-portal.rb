4633fcc7b75eaf0fd6043e129c1baab8f1892525
Tobias <tobiasgunn@gmail.com>
2026-03-15 22:00:37 -0500

    bump agent-portal to 1.0.2

--- Casks/agent-portal.rb
+++ Casks/agent-portal.rb
@@ -1,8 +1,8 @@
 cask "agent-portal" do
-  version "1.0.1"
-  sha256 "cfe07a63afbaad7464ccd88f9f1467a31c7d06d6d45ea378284e049a2ba1c918"
+  version "1.0.2"
+  sha256 "2acc00e68d5d2b7091bda27f315e330db258b24b63c23627ff84f2be900cef14"
 
-  url "https://github.com/tobsai/agent-portal-desktop/releases/download/v#{version}/Lewis-1.0.0.dmg"
+  url "https://github.com/tobsai/agent-portal-desktop/releases/download/v#{version}/Lewis-#{version}.dmg"
   name "Agent Portal"
   desc "Agent Portal — Desktop Chat for macOS"
   homepage "https://talos.mtree.io"
